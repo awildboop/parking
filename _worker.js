c4fa2d5be4475b86e29be6e48dd37868d719762a
Zander <me@boop.best>
2023-09-22 22:31:33 -0500

    Cloudflare makes my head hurt

--- _worker.js
+++ _worker.js
@@ -1,3 +1,11 @@
+class DomainLoader {
+    element(element) {
+        console.log(element)
+        // element.replace(default_header);
+    }
+}
+
+
 export default {
     async fetch(request, env) {
       const url = new URL(request.url);
@@ -5,13 +13,16 @@ export default {
       // Without this, the Worker will error and no assets will be served.
       return env.ASSETS.fetch(request);
     },
+
+    async handleRequest(req) {
+        const res = await fetch(req);
+        return new HTMLRewriter().on('h1', new DomainLoader()).transform(res);
+    },
   }
   
 /*
 
-
 class DomainLoader {
-
     element(element) {
         console.log(element)
         // element.replace(default_header);
