8a3513703da92feca8a7495f432f60ddcba07d1e
Zander <me@boop.best>
2023-09-22 23:42:48 -0500

    Finally done!

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,14 @@
 export const onRequestGet = async ({ request, next }) => {
     console.log(JSON.stringify(request))
-    try{
-      // Get the static asset response
-      const response = await next()
-        
-      // Find the placeholder in our static asset
-      return new HTMLRewriter().on('h1', {
-        // And act on the element
-        element(element) {
-          // https://developers.cloudflare.com/workers/runtime-apis/html-rewriter#methods
-          element.setInnerContent(request.headers.get("Host"), { html: true })
-        }
-      }).transform(response)
-      } catch (thrown){
-          return new Response(thrown);
-      }
+    try {
+        const response = await next()
+        return new HTMLRewriter().on('h1', {
+            element(element) {
+                // https://developers.cloudflare.com/workers/runtime-apis/html-rewriter#methods
+                element.setInnerContent(request.headers.get("Host"), { html: true })
+            }
+        }).transform(response)
+    } catch (thrown) {
+        return new Response(thrown);
     }
+}
