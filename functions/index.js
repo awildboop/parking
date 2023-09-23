a820d3468dedd9b1f4a331afd2e2c79cc0db8e3c
Zander <me@boop.best>
2023-09-22 22:44:26 -0500

    cloudflare makes my head hurt v5

--- functions/index.js
+++ functions/index.js
@@ -1,66 +1,17 @@
-export default {
-    async fetch(request) {
-        class DomainLoader {
-            element(element) {
-                console.log(element)
-                // element.replace(default_header);
-            }
+export const onRequestGet = async ({ request, next }) => {
+    try{
+      // Get the static asset response
+      const response = await next()
+        
+      // Find the placeholder in our static asset
+      return new HTMLRewriter().on('h1', {
+        // And act on the element
+        element(element) {
+          // https://developers.cloudflare.com/workers/runtime-apis/html-rewriter#methods
+          element.setInnerContent("replaced!", { html: true })
         }
-
-        const rewriter = new HTMLRewriter()
-            .on("h1", new DomainLoader());
-
-        const res = await fetch(request);
-        const contentType = res.headers.get("Content-Type");
-
-        if (contentType.startsWith("text/html")) {
-            return rewriter.transform(res);
-        } else {
-            return res;
-        }
+      }).transform(response)
+      } catch (thrown){
+          return new Response(thrown);
+      }
     }
-}
-
-/*
-class DomainLoader {
-    element(element) {
-        console.log(element)
-        // element.replace(default_header);
-    }
-}
-
-export default {
-    async fetch(request, env) {
-      const url = new URL(request.url);
-      // Otherwise, serve the static assets.
-      // Without this, the Worker will error and no assets will be served.
-      return env.ASSETS.fetch(request);
-    },
-
-    async handleRequest(req) {
-        const res = await fetch(req);
-        return new HTMLRewriter().on('h1', new DomainLoader()).transform(res);
-    },
-  }
-*/
-
-/* -------------------- */
-
-/*
-class DomainLoader {
-    element(element) {
-        console.log(element)
-        // element.replace(default_header);
-    }
-}
-
-async function handleRequest(req) {
-    const res = await fetch(req);
-    return new HTMLRewriter().on('h1', new DomainLoader()).transform(res);
-}
-
-addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request));
-})
-
-*/
